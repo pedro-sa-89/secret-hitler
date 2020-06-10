7a8342667fd626bb54aa874910117655306c35fa
Pedro Sá <git@pedro-sa.com>
2020-06-10 22:02:04 +0100

    cut the reference when save/load from the database

--- repository/database.js
+++ repository/database.js
@@ -4,11 +4,11 @@ const database = {};
 
 function save(collection, key, document) {
     database[collection] = _.defaultTo(database[collection], {})
-    database[collection][key] = document
+    database[collection][key] = _.cloneDeep(document)
 }
 
 function get(collection, key) {
-    return _.defaultTo(database[collection], {})[key]
+    return _.cloneDeep(_.defaultTo(database[collection], {})[key])
 }
 
 module.exports = {
